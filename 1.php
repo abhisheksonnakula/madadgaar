a9a1708a1f652b2c1a9b23056d5792cc88909849
Abhishek Sonnakula <sonnakulaabhishek98@gmail.com>
2018-06-03 12:22:54 +0530

    Update 1.php

--- 1.php
+++ 1.php
@@ -6,3 +6,8 @@ $description = $_GET['description'];
 $conn = mysqli_connect('localhost', 'root', '12345', 'madadgaar');
 
 $insert_query = "INSERT INTO workspace('statement','description') values('$statement','$description')";
+if (mysqli_query($conn, $insert_query)) {
+    echo 'Your Statement Submitted Successfully';
+} else {
+    echo'Cannot Add Statement';
+};
